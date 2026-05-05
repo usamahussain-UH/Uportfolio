eb3fa2899a952747daeddf3190f1fa5111073877
Amal Usama <265059952+usamahussain-UH@users.noreply.github.com>
2026-05-05 08:39:05 +0100

    neraly there

--- src/testing.tsx
+++ src/testing.tsx
@@ -148,3 +148,48 @@ module.exports = {
   },
   // Other options...
 };
+function myCoolFunction() {
+  if (arguments.length == 2 && !Array.isArray(arguments[1])) {
+    var f = arguments[0];
+    var arr = arguments[1];
+    // ...
+  }
+  // ...
+}
+myCoolFunction(
+  function (x) {
+    console.log(x);
+  },
+  [1, 2, 3, 4]
+);
+myCoolFunction(
+  function (x) {
+    console.log(x);
+  },
+  1,
+  2,
+  3,
+  4
+);function myCoolFunction() {
+  if (arguments.length == 2 && !Array.isArray(arguments[1])) {
+    var f = arguments[0];
+    var arr = arguments[1];
+    // ...
+  }
+  // ...
+}
+myCoolFunction(
+  function (x) {
+    console.log(x);
+  },
+  [1, 2, 3, 4]
+);
+myCoolFunction(
+  function (x) {
+    console.log(x);
+  },
+  1,
+  2,
+  3,
+  4
+);
