b81f147735519f505ffd0791e5150cc6b3a9e201
Amal Usama <265059952+usamahussain-UH@users.noreply.github.com>
2026-05-06 08:39:52 +0100

    so

--- src/testing.tsx
+++ src/testing.tsx
@@ -193,3 +193,49 @@ myCoolFunction(
   3,
   4
 );
+class Point {
+  constructor(public x, public y) {}
+  getDistance(p: Point) {
+    let dx = p.x - this.x;
+    let dy = p.y - this.y;
+    return Math.sqrt(dx ** 2 + dy ** 2);
+  }
+}
+// ...
+// Reopen the interface.
+interface Point {
+  distanceFromOrigin(): number;
+}
+Point.prototype.distanceFromOrigin = function () {
+  return this.getDistance({ x: 0, y: 0 });
+};class Point {
+  constructor(public x, public y) {}
+  getDistance(p: Point) {
+    let dx = p.x - this.x;
+    let dy = p.y - this.y;
+    return Math.sqrt(dx ** 2 + dy ** 2);
+  }
+}
+// ...
+// Reopen the interface.
+interface Point {
+  distanceFromOrigin(): number;
+}
+Point.prototype.distanceFromOrigin = function () {
+  return this.getDistance({ x: 0, y: 0 });
+};class Point {
+  constructor(public x, public y) {}
+  getDistance(p: Point) {
+    let dx = p.x - this.x;
+    let dy = p.y - this.y;
+    return Math.sqrt(dx ** 2 + dy ** 2);
+  }
+}
+// ...
+// Reopen the interface.
+interface Point {
+  distanceFromOrigin(): number;
+}
+Point.prototype.distanceFromOrigin = function () {
+  return this.getDistance({ x: 0, y: 0 });
+};
