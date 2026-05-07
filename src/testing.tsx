1ee6f7bcc7ede292bafb409da10453c6620b46ab
Amal Usama <265059952+usamahussain-UH@users.noreply.github.com>
2026-05-07 08:41:01 +0100

    last test

--- src/testing.tsx
+++ src/testing.tsx
@@ -239,3 +239,55 @@ interface Point {
 Point.prototype.distanceFromOrigin = function () {
   return this.getDistance({ x: 0, y: 0 });
 };
+
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var tsify = require("tsify");
+var paths = {
+  pages: ["src/*.html"],
+};
+gulp.task("copy-html", function () {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+});
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("copy-html"), function () {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["src/main.ts"],
+      cache: {},
+      packageCache: {},
+    })
+      .plugin(tsify)
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(gulp.dest("dist"));
+  })
+);var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var tsify = require("tsify");
+var paths = {
+  pages: ["src/*.html"],
+};
+gulp.task("copy-html", function () {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+});
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("copy-html"), function () {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["src/main.ts"],
+      cache: {},
+      packageCache: {},
+    })
+      .plugin(tsify)
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(gulp.dest("dist"));
+  })
+);
